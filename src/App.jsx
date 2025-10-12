df17440ddeb15ba14cad1e261301d307c49af36f
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-10-12 21:11:40 +0300

    Add: navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { Navbar } from "./components/NavBar";
+
 export function App() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-[#efede3]  before:absolute before:inset-0 before:bg-[url('https://grainy-gradients.vercel.app/noise.svg')] before:opacity-40 before:pointer-events-none">
+      <header>
+        <Navbar />
+      </header>
+    </div>
+  );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+export function Navbar() {
+  return (
+    <nav className="flex justify-between items-center mx-10 h-20 border-b-1 text-2xl uppercase  ">
+      <a className="font-bold " href="#">
+        Logo
+      </a>
+      <ul className="flex justify-center items-center h-10 gap-[7vw] font-bold">
+        <li>
+          <a href="#" className="">
+            About me
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="#" className="">
+            FAQ
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
