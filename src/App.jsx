e874516f0c77daaaaadb369c30a2a42147be0716
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-11-03 13:23:03 +0300

    Add: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Navbar } from "./components/NavBar";
+import { Hero } from "./components/Hero";
 
 export function App() {
   return (
-    <div className="min-h-screen bg-[#efede3]  before:absolute before:inset-0 before:bg-[url('https://grainy-gradients.vercel.app/noise.svg')] before:opacity-40 before:pointer-events-none">
-      <header>
-        <Navbar />
-      </header>
+    <div className="relative">
+      <div className="min-h-screen bg-[#efede3]  before:absolute before:inset-0 before:bg-[url('https://grainy-gradients.vercel.app/noise.svg')] before:opacity-40 before:pointer-events-none">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Hero />
+        </main>
+      </div>
     </div>
   );
 }
