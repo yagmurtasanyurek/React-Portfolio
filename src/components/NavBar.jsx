a0e6c6c7e85785843a2477935802909cf7680e8a
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-12-15 22:30:12 +0300

    add:responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <nav className="flex justify-between items-center h-20 border-b-1 text-lg md:text-2xl lg:text-3xl mx-5 md:mx-10 uppercase  ">
+    <nav className="flex justify-between items-center h-20 border-b-1 text-lg max-[475px]:text-sm md:text-2xl lg:text-3xl mx-4 md:mx-10 uppercase  ">
       <a className="font-bold " href="#">
         Logo
       </a>
