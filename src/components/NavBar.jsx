ceb8a69c8ee9c68def04abebd74b6202de599a99
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-12-15 17:02:19 +0300

    add:responsive font size and gap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 export function Navbar() {
   return (
-    <nav className="flex justify-between items-center mx-10 h-20 border-b-1 text-2xl uppercase  ">
+    <nav className="flex justify-between items-center h-20 border-b-1 text-lg md:text-2xl lg:text-3xl mx-5 md:mx-10 uppercase  ">
       <a className="font-bold " href="#">
         Logo
       </a>
-      <ul className="flex justify-center items-center h-10 gap-[7vw] font-bold">
+      <ul className="flex justify-center items-center h-10 gap-[5vw] md:gap-[7vw] font-bold">
         <li>
           <a href="#" className="">
             About me
