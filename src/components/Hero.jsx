043d1d26acfc77d1aa621bc5bbd6886b2bfe9b26
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-12-15 16:26:00 +0300

    add:responsive design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,17 +2,17 @@ import YagmurPhoto from "../images/Yagmur.jpeg";
 import FlowerPng from "../images/png-flower-silhouette-art-splattered.png";
 export function Hero() {
   return (
-    <section className="flex justify-center items-center mt-10 px-6 pb-10">
-      <div className="flex justify-center gap-10  ">
+    <section className="flex justify-center items-center pt-24 pb-40 ">
+      <div className="flex flex-col md:flex-row justify-center gap-10 ">
         {/* first box */}
-        <div className="  bg-pink-900 min-w-2xs  md:min-w-lg relative ">
+        <div className=" bg-pink-900 min-w-2xs sm:min-w-lg md:min-w-2xl relative pb-10">
           {/* Background png */}
           <div
             className="absolute inset-0 top-11 bg-no-repeat bg-cover bg-center z-0"
             style={{ backgroundImage: `url(${FlowerPng})` }}
           ></div>
           <div className="flex flex-col justify-center items-center">
-            <h1 className="font-bold text-2xl md:text-3xl text-white text-center  uppercase pt-5 md:pt-10 z-50 ">
+            <h1 className="font-bold text-2xl md:text-3xl text-white text-center uppercase pt-5 md:pt-10 z-50 ">
               Yağmur
             </h1>
             <h1 className="font-bold text-2xl md:text-3xl text-white text-center uppercase pb-4 md:pb-8 z-50">
@@ -29,16 +29,16 @@ export function Hero() {
               </div>
             </div>
             {/* Pharagraph div here */}
-            <div className="z-50">
+            {/*     <div className="z-50">
               <p className="font-semibold text-xl md:text-2xl text-white text-center uppercase pb-10 ">
                 Lifelong Learner
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
         {/* second box */}
-        <div className=" bg-cyan-950 flex items-center justify-center w-32 md:w-48 z-10 p-10">
-          <h1 className="text-3xl md:text-4xl text-white font-semibold -rotate-90 uppercase whitespace-nowrap">
+        <div className="inline-flex items-center justify-center bg-cyan-950  md:w-48 z-10 p-10 ">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl text-white font-semibold md:-rotate-90 uppercase whitespace-nowrap">
             Front-end developer
           </h1>
         </div>
