75d9db05528308d96820744638f901e390ff74de
Yağmur Taşanyürek <yagmurtasanyurek@gmail.com>
2025-12-15 22:30:12 +0300

    Cherry-pick commit a0e6c6cadd:responsive design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <nav className="flex justify-between items-center mx-10 h-20 border-b-1 text-2xl uppercase  ">
+    <nav className="flex justify-between items-center h-20 border-b-1 text-lg max-[475px]:text-sm md:text-2xl lg:text-3xl mx-4 md:mx-10 uppercase  ">
       <a className="font-bold " href="#">
         Logo
       </a>
